14e0dd3951fc128a22d7c143957a0d4827bf6e31
minaripenguin <minaripenguin@users.noreply.github.com>
2023-01-17 01:56:55 +0800

    Launcher3: Implement translucent recents overview
    Signed-off-by: minaripenguin <minaripenguin@users.noreply.github.com>

--- src/com/android/launcher3/Utilities.java
+++ src/com/android/launcher3/Utilities.java
@@ -1125,8 +1125,7 @@ public final class Utilities {
    }
 
     public static int getRecentsOpacity(Context context) {
-        SharedPreferences prefs = LauncherPrefs.getPrefs(context.getApplicationContext());
-        return prefs.getInt(KEY_RECENTS_OPACITY, 40);
+        return 0;
     }
 
     public static int getAllAppsOpacity(Context context) {
